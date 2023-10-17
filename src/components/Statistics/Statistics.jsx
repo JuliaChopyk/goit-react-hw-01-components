5df66d128389a70dc43b5bd4fad3f4c280cde5a5
JuliaChopyk <chopyk.julia@gmail.com>
2023-10-17 20:37:31 +0300

    Hw part 1 (profile and statistics)

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import css from './StatisticStyles.css';
+import { randomColor } from './colorRandomizer';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      <h2 className={css.title}>Upload stats</h2>
+
+      <ul className={css.statisticsList}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            key={id}
+            className={css.item}
+            style={{ backgroundColor: randomColor() }}
+          >
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.array,
+};
